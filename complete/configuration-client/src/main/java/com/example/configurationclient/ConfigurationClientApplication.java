94c2ce0114bef6dd75b94fda78b2b535d484fb63
aristotle0x01 <aristotle0x01@protonmail.com>
2024-05-18 17:12:03 +0800

    cglib output

--- complete/configuration-client/src/main/java/com/example/configurationclient/ConfigurationClientApplication.java
+++ complete/configuration-client/src/main/java/com/example/configurationclient/ConfigurationClientApplication.java
@@ -18,11 +18,13 @@ package com.example.configurationclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.DebuggingClassWriter;
 
 @SpringBootApplication
 public class ConfigurationClientApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"cglib");
 		SpringApplication.run(ConfigurationClientApplication.class, args);
 	}
 }
